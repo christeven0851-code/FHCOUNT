9ffb79554c8518e2f7caef02bd742e8ee04419e8
christeven0851-code <christeven0851@gmail.com>
2026-03-02 16:33:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,8 @@ up_extra_total = max(0, lim_p20 - lim_b1)
 lim_b6 = labor_round(all_denominator * (rate + use_extra_rate + 0.05)) - labor_round(all_denominator * (rate + use_extra_rate))
 # 加薪案人數上限      
 lim_b7 = min ( labor_round(all_denominator * (0.10)) , 
-              labor_round(all_denominator * (rate + min ( use_extra_rate + 0.10, 0.45 )))- labor_round(all_denominator * (rate + use_extra_rate) ))
+               labor_round(all_denominator * (rate + min ( use_extra_rate + 0.10, 0.45 ))) - (b1 + b_extra_total + val),
+               labor_round(all_denominator * (0.5)) - sum_all_foreign + val )
 
 
 # lim_b7 = labor_round(all_denominator * (rate + use_extra_rate + 0.10)) - labor_round(all_denominator * (rate + use_extra_rate))
